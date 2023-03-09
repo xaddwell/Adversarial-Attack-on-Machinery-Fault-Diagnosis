1d437efbdeeb448d37d6c242a04e95b3b3147cb6
xaddwell <72803316+xaddwell@users.noreply.github.com>
2023-03-09 19:24:01 +0800

    Add files via upload

---
+++ MyAttackModel/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=UTF-8
+"""
+@Author: linna
+@LastEditors: linna
+@Description: 
+@Date: 2019-09-0313:38:17
+@LastEditTime: 2020-09-23 13:38:22
+"""
+from .fgsm import FGSM
+from .pgd import PGD
+from .deepfool import DEEPFOOL
+from .cw2 import CW2
+
